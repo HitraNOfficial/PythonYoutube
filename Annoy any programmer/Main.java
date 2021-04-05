3df6b79d980b9ee22b69ea484a0797eacaae3c1a
HitraNOfficial <58542638+HitraNOfficial@users.noreply.github.com>
2021-04-05 17:01:17 +0300

    Create Main.java

---
+++ Annoy any programmer/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String helloWorld = "Hello World";
+        System.out.println(helloWorld);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
